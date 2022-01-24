a9c6fec7818766aa72983f92302bb80770b02d1b
Martinits <mamartinitramam@163.com>
2022-01-24 20:42:44 +0800

    update ranger config: add yank_content command

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -1,9 +1,11 @@
 from __future__ import (absolute_import, division, print_function)
 
-from subprocess import check_output
 import os
-
+import subprocess
 from ranger.api.commands import Command
+from ranger.container.file import File
+from ranger.ext.get_executables import get_executables
+
 
 class mkcd(Command):
     """
@@ -50,10 +52,12 @@ class mkcd(Command):
 #             arguments = 'source {} && _z -e {}'.format(
 #                 z_loc, " ".join(self.args[1:]))
 #             cmd = ['zsh', '-c', arguments]
-#             directory = check_output(cmd).decode("utf-8").rstrip("\n")
+#             directory = subprocess.check_output(cmd).decode("utf-8").rstrip("\n")
+#
 #             self.fm.execute_console("cd " + directory)
 #         except Exception as e:
 #             raise Exception("Directory not found")
+
 
 class fzf_select(Command):
     """
@@ -114,3 +118,41 @@ class fzf_select(Command):
                 self.fm.cd(selected)
             else:
                 self.fm.select_file(selected)
+
+
+class yank_content(Command):
+    """
+    Copy the content of image file and text file with xclip
+    """
+
+    def execute(self):
+        if 'xclip' not in get_executables():
+            self.fm.notify('xclip is not found.', bad=True)
+            return
+
+        arg = self.rest(1)
+        if arg:
+            if not os.path.isfile(arg):
+                self.fm.notify('{} is not a file.'.format(arg))
+                return
+            file = File(arg)
+        else:
+            file = self.fm.thisfile
+            if not file.is_file:
+                self.fm.notify('{} is not a file.'.format(file.relative_path))
+                return
+
+        relative_path = file.relative_path
+        cmd = ['xclip', '-selection', 'clipboard']
+        if not file.is_binary():
+            with open(file.path, 'rb') as fd:
+                subprocess.check_call(cmd, stdin=fd)
+        elif file.image:
+            cmd += ['-t', file.mimetype, file.path]
+            subprocess.check_call(cmd)
+            self.fm.notify('Content of {} is copied to x clipboard'.format(relative_path))
+        else:
+            self.fm.notify('{} is not an image file or a text file.'.format(relative_path))
+
+    def tab(self, tabnum):
+        return self._tab_directory_content()
