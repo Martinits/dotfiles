3dbbf992fe9fee5b92dd9c11acbfcf8751f2fc33
Martinits <mamartinitramam@163.com>
2022-05-29 12:24:41 +0800

    add surfingkeys config

---
+++ .config/surfingkeys/config.js
@@ -0,0 +1,52 @@
+// set theme
+settings.theme = `
+.sk_theme {
+    font-family: Input Sans Condensed, Charcoal, sans-serif;
+    font-size: 10pt;
+    background: #24272e;
+    color: #abb2bf;
+}
+.sk_theme tbody {
+    color: #fff;
+}
+.sk_theme input {
+    color: #d0d0d0;
+}
+.sk_theme .url {
+    color: #61afef;
+}
+.sk_theme .annotation {
+    color: #56b6c2;
+}
+.sk_theme .omnibar_highlight {
+    color: #528bff;
+}
+.sk_theme .omnibar_timestamp {
+    color: #e5c07b;
+}
+.sk_theme .omnibar_visitcount {
+    color: #98c379;
+}
+.sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
+    background: #303030;
+}
+.sk_theme #sk_omnibarSearchResult ul li.focused {
+    background: #3e4452;
+}
+#sk_status, #sk_find {
+    font-size: 20pt;
+}`;
+// click `Save` button to make above settings to take effect.</ctrl-i></ctrl-y>
+
+settings.hintAlign = "left";
+api.map('<Ctrl-[>', '<Esc>');
+api.removeSearchAlias('w');
+api.removeSearchAlias('e');
+api.removeSearchAlias('s');
+api.removeSearchAlias('y');
+api.addSearchAlias('u', 'github', 'https://github.com/search?q=', 's', null, function(response) {
+    var res = JSON.parse(response.text);
+    return res.map(function(r){
+        return r.phrase;
+    });
+});
