20a7c0daa87e975e0a038c2c5227e0fe2ebab1c8
Martinits <mamartinitramam@163.com>
2023-06-07 15:06:25 +0800

    update nvim config: nerd-fonts

--- .config/nvim/lua/lsp-init.lua
+++ .config/nvim/lua/lsp-init.lua
@@ -148,7 +148,7 @@ vim.diagnostic.config({
 })
 
 -- icons in signcolumn
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
