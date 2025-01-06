30a07f5c7562b7a5a04332a04608099737f3d6f3
Martinits <mamartinitramam@163.com>
2025-01-06 19:45:52 +0800

    yazi: add bookmarks; nested warning

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,1 +1,25 @@
 require("git"):setup()
+
+if os.getenv("YAZI_LEVEL") > "1" then
+  ya.notify {
+    title = "Nested Yazi",
+    content = "You are in a nested Yazi session",
+    timeout = 2,
+  }
+end
+
+require("bookmarks"):setup({
+	last_directory = { enable = true, persist = true },
+	persist = "vim",
+	desc_format = "full",
+	file_pick_mode = "hover",
+	notify = {
+		enable = true,
+		timeout = 1,
+		message = {
+			new = "New bookmark '<key>' -> '<folder>'",
+			delete = "Deleted bookmark in '<key>'",
+			delete_all = "Deleted all bookmarks",
+		},
+	},
+})
